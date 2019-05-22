f9840686472bcfef8c8c9c241c6888e911010696
vincentdu101 <vincentdu101@gmail.com>
2019-05-21 22:19:44 -0700

    adding animations, styling, and event listening to bar graph

--- src/app/home/home.bar.graph.component.ts
+++ src/app/home/home.bar.graph.component.ts
@@ -10,13 +10,14 @@ export class HomeBarGraphComponent implements AfterContentInit {
 
     @ViewChild("bar-chart") barChart: ElementRef; 
     width = 900;
-    margin = {top: 30, right: 0, bottom: 10, left: 50};
+    margin = {top: 30, right: 20, bottom: 10, left: 100};
     data = [];
     loader = true;
     private x: d3.ScaleLinear<number, number>;
     private y: d3.ScaleBand<string>;
     private xAxis;
     private yAxis;
+    private colorScale;
 
     constructor() {
         this.determineHeight = this.determineHeight.bind(this);
@@ -27,10 +28,16 @@ export class HomeBarGraphComponent implements AfterContentInit {
 
     ngAfterContentInit() {
         this.data = [
-            {year: 1990, count: 1000},
-            {year: 2000, count: 1060},
-            {year: 2010, count: 2000},
-            {year: 2020, count: 6000}
+            {name: "TypeScript", description: 1000, body: "blue"},
+            {name: "D3", description: 1060, body: "green"},
+            {name: "JavaScript", description: 2000, body: "yellow"},
+            {name: "Java", description: 6000, body: "black"},
+            {name: "Python", description: 1000, body: "red"},
+            {name: "Ruby on Rails", description: 1060, body: "orange"},
+            {name: "AI", description: 2000, body: "pink"},
+            {name: "Unity3D", description: 6000, body: "aqua"},
+            {name: "C#", description: 2000, body: "purple"},
+            {name: "CSS3", description: 6000, body: "gray"}
         ];
         this.createBarChart();
     }
@@ -44,27 +51,40 @@ export class HomeBarGraphComponent implements AfterContentInit {
 
     private getXScaleLinear(): d3.ScaleLinear<number, number> {
         return d3.scaleLinear()
-                .domain([0, d3.max(this.data, d => d.count)])
+                .domain([0, d3.max(this.data, d => d.description + 1000)])
                 .range([this.margin.left, this.width]);
     }
 
     private getYScaleLinear(): d3.ScaleBand<string> {
         return d3.scaleBand()
-                .domain(this.data.map(d => d.year))
+                .domain(this.data.map(d => d.name))
                 .range([this.margin.top, this.determineHeight() - this.margin.bottom])
                 .padding(0.1);
     }
 
-    private getXAxisHeight() {
-        return `translate(0,${this.determineHeight() - this.margin.bottom})`;
-    }
+    private generateRectangles(svg) {
+        let rect = svg.append("g")
+            .selectAll("rect")
+                .data(this.data)
+            .enter().append("rect")
+                .style("pointer-events", "all")
+                .attr("id", (d) => d.body)
+                .attr("width", 0)
+                .attr("x", this.x(0))
+                .attr("y", d => this.y(d.name))
+                .attr("height", this.y.bandwidth());
+        
+        rect.transition()
+            .attr("fill", (d, i) => this.data[i].body)
+            .attr("width", d => this.x(d.description) - this.x(0))
+            .delay((d, i) => i * 300);
 
-    private setupXGridlines(svg) {
-        svg.append("g")
-            .attr("class", "grid")
-            .attr("transform", this.getXAxisHeight())
-            .call(d3.axisTop(this.x));
-            // .tickFormat(""));
+        rect.on("mouseover", (element) => {
+            d3.select("#" + element.body).attr("fill", "red");
+        }).on("mouseout", (element) => {
+            d3.select("#" + element.body)
+                .attr("fill", element.body);
+        });
     }
 
     private createBarChart() {
@@ -81,20 +101,18 @@ export class HomeBarGraphComponent implements AfterContentInit {
             .call(d3.axisTop(this.x).ticks(10))
             .call(g => g.select(".domain").remove());
 
-        this.yAxis = (g) => g
-            .attr("transform", `translate(${this.margin.left}, 0)`)
-            .call(d3.axisLeft(this.y).tickSizeOuter(0));
+        this.yAxis = (g) => {
+            g.attr("transform", `translate(${this.margin.left}, 0)`)
+            .style("font-size", "12px")
+            .call(d3.axisLeft(this.y).tickSizeOuter(0))
+            .selectAll("path")
+                .attr("display", "none");
+            g.selectAll("line")
+            .attr("display", "none");   
+        }
 
-        svg.append("g")
-            .attr("fill", "steelblue")
-            .selectAll("rect")
-                .data(this.data)
-            .enter().append("rect")
-                .attr("x", this.x(0))
-                .attr("y", d => this.y(d.year))
-                .attr("width", d => this.x(d.count) - this.x(0))
-                .attr("height", this.y.bandwidth());
-        
+        this.generateRectangles(svg);
+
         svg.append("g")
             .attr("fill", "white")
             .attr("text-anchor", "end")
@@ -102,13 +120,11 @@ export class HomeBarGraphComponent implements AfterContentInit {
             .selectAll("text")
                 .data(this.data)
             .enter().append("text")
-                .attr("x", d => this.x(d.count) - 4)
-                .attr("y", d => this.y(d.year) + this.y.bandwidth() / 2)
+                .attr("x", d => this.x(d.description) - 4)
+                .attr("y", d => this.y(d.name) + this.y.bandwidth() / 2)
                 .attr("dy", "0.35em")
-                .text(d => d.count);    
+                .text(d => d.description);    
         
-        svg.append("g").call(this.xAxis);
         svg.append("g").call(this.yAxis);
-        this.setupXGridlines(svg);
     }
 }
